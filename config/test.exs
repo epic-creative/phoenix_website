72e8948510ec287864f424883cbe24d7f0c4225b
Mike Hostetler <mike.hostetler@raise.com>
2018-09-28 17:37:28 -0500

    Updated test env credentials

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :phoenix_website, PhoenixWebsite.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "test",
+  password: "test",
   database: "phoenix_website_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
